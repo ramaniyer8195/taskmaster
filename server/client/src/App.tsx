abd68ee544f97bc1cfadfff3c0a103602609898b
Raman Iyer <ramaniyer8195@gmail.com>
2024-08-08 01:10:22 +0530

    Added more boilerplate code for frontend

--- server/client/src/App.tsx
+++ server/client/src/App.tsx
@@ -1,7 +1,31 @@
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import OtpVerify from "./pages/OtpVerify";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import AppBar from "./components/app-bar/AppBar";
 
 function App() {
-  return <div>Hello World!</div>;
+  const theme = createTheme({});
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/otpVerify" element={<OtpVerify />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ server/client/src/pages/OtpVerify.tsx
@@ -0,0 +1,5 @@
+const OtpVerify = () => {
+  return <div>OtpVerify</div>;
+};
+
+export default OtpVerify;
