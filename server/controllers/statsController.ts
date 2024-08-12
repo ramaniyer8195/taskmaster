917e18b84c1d2c3a708bc99bdf1899edf4dfae56
Raman Iyer <ramaniyer8195@gmail.com>
2024-08-12 22:22:29 +0530

    Added stats route implementation (#6)

--- server/controllers/statsController.ts
+++ server/controllers/statsController.ts
@@ -1,5 +1,15 @@
 import { RequestHandler } from "express";
+import { GetStatsRes } from "../interfaces/controllers";
+import User from "../models/User";
+import Note from "../models/Note";
+import Todo from "../models/Todo";
 
-export const getStats: RequestHandler = (req, res) => {
-  res.send("stats");
+export const getStats: RequestHandler<{}, GetStatsRes> = async (req, res) => {
+  const userCount = (await User.countDocuments({})) || 0;
+  const notesCount = (await Note.countDocuments({})) || 0;
+  const todosCount = (await Todo.countDocuments({})) || 0;
+
+  res.status(200).json({
+    data: { users: userCount, notes: notesCount, todos: todosCount },
+  });
 };
