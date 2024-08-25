d6075b460b1fb2f561d2923f058847f31ddeecf5
Raman Iyer <ramaniyer8195@gmail.com>
2024-08-25 22:16:26 +0530

    Integrated everything inside dashboard page

--- server/client/src/pages/Dashboard.tsx
+++ server/client/src/pages/Dashboard.tsx
@@ -1,5 +1,49 @@
+import { MouseEvent, useState } from "react";
+import { Menu, TopicItem } from "@/interfaces/dashboard";
+import Sidebar from "@/components/dashboard/Sidebar";
+import HomeTab from "@/components/dashboard/HomeTab";
+import FavouritesTab from "@/components/dashboard/FavouritesTab";
+import RecentlyDeletedTab from "@/components/dashboard/RecentlyDeletedTab";
+import ArchiveTab from "@/components/dashboard/ArchiveTab";
+import SettingsTab from "@/components/dashboard/SettingsTab";
+import { TOPIC_COLORS } from "@/constants/constants";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [selected, setSelected] = useState(Menu.HOME);
+  const [topics, setTopics] = useState<TopicItem[]>([
+    { name: "Personal", colorClass: TOPIC_COLORS.EMERALD_GREEN },
+    { name: "Business", colorClass: TOPIC_COLORS.CHARCOAL },
+  ]);
+
+  const handleMenuChange = (e: MouseEvent<HTMLDivElement>) => {
+    setSelected(e.currentTarget.innerText as Menu);
+  };
+
+  const handleSignOut = () => {
+    console.log("Sign out clicked!");
+  };
+
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-between">
+      <div className="min-w-[15%] pt-5 bg-muted relative">
+        <Sidebar
+          handleMenuChange={handleMenuChange}
+          selected={selected}
+          topics={topics}
+          handleSignOut={handleSignOut}
+        />
+      </div>
+      <div className="w-[85%] h-full pt-5 px-5">
+        {selected === Menu.HOME && <HomeTab topics={topics} items={[]} />}
+        {selected === Menu.FAVOURITES && <FavouritesTab items={[]} />}
+        {selected === Menu.RECENTLY_DELETED && (
+          <RecentlyDeletedTab items={[]} />
+        )}
+        {selected === Menu.ARCHIVE && <ArchiveTab items={[]} />}
+        {selected === Menu.SETTINGS && <SettingsTab />}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
