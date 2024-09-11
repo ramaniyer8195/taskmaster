235605e49b45b225b79691fc3787487bdf4ad534
Raman Iyer <ramaniyer8195@gmail.com>
2024-09-11 23:23:34 +0530

    404 page added

--- server/client/src/pages/NotFound.tsx
+++ server/client/src/pages/NotFound.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import notFound from "../assets/404_illustration.svg";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <img src={notFound} alt="" className="h-[650px] w-full" />
+      <div className="font-display text-3xl font-bold text-center">
+        404 Not found
+      </div>
+      <div className="text-xl font-bold text-center">
+        Take me{" "}
+        <Link to="/" className="text-primary underline">
+          home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
